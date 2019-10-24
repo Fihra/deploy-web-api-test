b2e6bdab140ba9ed73c69264ef818f473315985b
Fabian Fabro <fabrofabian@gmail.com>
2019-10-24 09:27:48 -0700

    First commit

---
+++ WebAPIDemo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IEnumerable<Product> GetItems()
+        {
+            IList<Product> products = new List<Product>
+            {
+                new Product
+                {
+                    productName = "Biscuits",
+                    manufacturingYear = 2018,
+                    brandName="ParleG"
+                },
+                new Product
+                {
+                    productName = "Cars",
+                    manufacturingYear = 2018,
+                    brandName="BMW"
+                },
+                new Product
+                {
+                    productName="Cars",
+                    manufacturingYear=2018,
+                    brandName="Mercedese"
+                },
+                new Product
+                {
+                    productName="Brush",
+                    manufacturingYear=2017,
+                    brandName="Colgate"
+                }
+            };
+            return products;
+        }
+    }
+}
